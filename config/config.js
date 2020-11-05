9a2e911a65cc9577672c9fea302e93f72eb5dace
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-11-04 22:08:14 -0600

    heroku stuff

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  "development": {
+    "username": "z3nf1e0gqc2zskf1",
+    "password": yvq22qw0z8tulnxa,
+    "database": "ljj5g5xqapnwmtqo",
+    "host": "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 require("dotenv").config();
 
@@ -13,7 +13,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, process.env.MYSQL_PW, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
